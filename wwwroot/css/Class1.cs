1f822d026e280d6e984068b90de40ed1cc37cdd1
BrodyWilson97 <153255880+BrodyWilson97@users.noreply.github.com>
2024-03-15 10:40:16 -0600

    Add files via upload

---
+++ wwwroot/css/Class1.cs
@@ -0,0 +1,16 @@
+﻿public class Flight
+{
+    public string FlightCode { get; set; }
+    public string Airline { get; set; }
+    public string OriginAirportCode { get; set; }
+    public string DestinationAirportCode { get; set; }
+    public string DayOfWeek { get; set; }
+    public string DepartureTime { get; set; }
+    public decimal Cost { get; set; }
+}
+public class Airport
+{
+    public string AirportCode { get; set; }
+    public string AirportName { get; set; }
+}
+
